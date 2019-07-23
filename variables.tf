95228a0696e8bcf1ca2a5d0cf93d31874409d64b
jeremy woo <kepungmoin@hotmail.com>
2019-07-23 09:58:25 +0800

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+    default = "australieeast"
+}
+
+variable "environment" {}
+
+variable "cosmosdbaccount" {}
+
+variable "keyvaultname" {}
+
+variable "resource_group_name" {}
+
+variable "eventhub_namespace_name" {}
+
+variable "workspacename" {
+  
+}
+
+
